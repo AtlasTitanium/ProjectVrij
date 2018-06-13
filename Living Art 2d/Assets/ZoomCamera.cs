67315aa23c4e8e0075e1492fddd53d74b6f95943
PepijnKok <AtlasTheTitan77@gmail.com>
2018-06-13 13:06:02 +0200

    yes

---
+++ Living Art 2d/Assets/ZoomCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ZoomCamera : MonoBehaviour {
+	private float fadeTransperency = 1.0f;
+	private Image fadeImage;
+	void Awake () {
+		fadeImage = this.GetComponent<Image>();
+	}
+	void Update () {
+		fadeImage.color = new Color(0,0,0,fadeTransperency);
+		fadeTransperency -= 0.4f * Time.deltaTime;
+		if(fadeTransperency <= 0.05f){
+			fadeTransperency = 0;
+			this.enabled = false;
+		}
+	}
+	
+}
