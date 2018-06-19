6a92d278cf73848c2dd2fa767c5faf7080b150f9
PepijnKok <AtlasTheTitan77@gmail.com>
2018-06-19 23:01:54 +0200

    boii

---
+++ Living Art 2d/Assets/MonsterBaby.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MonsterBaby : MonoBehaviour {
+
+	public Image ShadowText;
+	public Text UnderText;
+	public bool blocked = false;
+	public GameObject GumObject;
+	public float speed;
+	public bool talkedToPlayer = false;
+	private float transparesy = 0.1f;
+	private bool howfast = false;
+
+	public Sprite[] idleSprites;
+
+	private bool waitForNextFrame = false;
+	private int i = -1;
+
+	void Update () {
+		if(!waitForNextFrame){
+			i += 1;
+			if(i == idleSprites.Length){
+				i = 0;
+			}
+			this.GetComponent<SpriteRenderer>().sprite = idleSprites[i];
+			StartCoroutine(WaitMonster());
+			Debug.Log("SpriteChange");
+			waitForNextFrame = true;
+		}
+		if(talkedToPlayer){
+			if(GumObject == null){
+				//anim.SetBool("Movin",false);
+				GumObject = GameObject.FindGameObjectWithTag("HoldingGum");
+			} else {
+				float step = speed * Time.deltaTime;
+				this.transform.position = Vector2.MoveTowards(this.transform.position, GumObject.transform.position, step);
+				this.transform.localPosition = new Vector3(this.transform.localPosition.x,this.transform.localPosition.y,1);
+				//anim.SetBool("Movin",true);
+				Debug.Log("following Gum");
+				if(GumObject.tag == "Gum"){
+					//anim.SetBool("Movin",false);
+					GumObject = null;
+				}
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag == "SpiderMonster"){
+			Debug.Log("There's a monster");
+			other.GetComponent<SpiderMonster>().monster = this.transform.gameObject;
+			blocked = true;
+			talkedToPlayer = false;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if(other.tag == "SpiderMonster"){
+			Debug.Log("There's a monster");
+			other.GetComponent<SpiderMonster>().monster = null;
+			blocked = false;
+		}
+	}
+
+	IEnumerator WaitMonster()
+    {
+        yield return new WaitForSeconds(0.4f);
+        waitForNextFrame = false;
+    }
+}
--- Living Art 2d/Assets/Rain.cs
+++ Living Art 2d/Assets/Rain.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Rain : MonoBehaviour{
 	ParticleSystem ps;
+    private GameObject Spoodmonst;
 
     // these lists are used to contain the particles which match
     // the trigger conditions each frame.
@@ -22,26 +23,53 @@ public class Rain : MonoBehaviour{
         int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
         int numExit = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
 
-        // iterate through the particles which entered the trigger and make them red
-        for (int i = 0; i < numEnter; i++)
-        {
-			Debug.Log("enter monster");
-			col.gameObject.GetComponent<gravityMonster>().blocked = true;
-            col.gameObject.GetComponent<gravityMonster>().talkedToPlayer = false;
-			col.gameObject.transform.localPosition = new Vector2(col.gameObject.transform.localPosition.x-0.5f,col.gameObject.transform.localPosition.y);
-            col.gameObject.GetComponent<gravityMonster>().anim.SetBool("Scared",true);
-        }
+        if(col.gameObject.tag == "Monster"){
+             // iterate through the particles which entered the trigger and make them red
+            for (int i = 0; i < numEnter; i++)
+            {
+                Debug.Log("enter monster");
+                col.gameObject.GetComponent<gravityMonster>().blocked = true;
+                col.gameObject.GetComponent<gravityMonster>().talkedToPlayer = false;
+                col.gameObject.transform.localPosition = new Vector2(col.gameObject.transform.localPosition.x-0.5f,col.gameObject.transform.localPosition.y);
+                col.gameObject.GetComponent<gravityMonster>().anim.SetBool("Scared",true);
+            }
 
-        // iterate through the particles which exited the trigger and make them green
-        for (int i = 0; i < numExit; i++)
-        {
-			Debug.Log("leave monster");
-			col.gameObject.GetComponent<gravityMonster>().blocked = false;
+            // iterate through the particles which exited the trigger and make them green
+            for (int i = 0; i < numExit; i++)
+            {
+                Debug.Log("leave monster");
+                col.gameObject.GetComponent<gravityMonster>().blocked = false;
+            }
+        }
+        if(col.gameObject.tag == "SpiderMonster"){
+             // iterate through the particles which entered the trigger and make them red
+            for (int i = 0; i < numEnter; i++)
+            {
+                Spoodmonst = col.gameObject;
+                Debug.Log("enter Spooder");
+                col.gameObject.GetComponent<SpiderMonster>().GumObject = null;
+                col.gameObject.GetComponent<Collider2D>().enabled = false;
+                col.gameObject.transform.localPosition = new Vector2(col.gameObject.transform.localPosition.x+0.5f,col.gameObject.transform.localPosition.y);
+            }
+
+            // iterate through the particles which exited the trigger and make them green
+            for (int i = 0; i < numExit; i++)
+            {
+                Debug.Log("leave Spooder");
+                StartCoroutine(Fast());
+            }
         }
 
         // re-assign the modified particles back into the particle system
         ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
         ps.SetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
 		
+    }
+
+    IEnumerator Fast()
+    {
+        yield return new WaitForSeconds(1f);
+        Spoodmonst.gameObject.GetComponent<Collider2D>().enabled = true;
+        Spoodmonst = null;
     }
 }
