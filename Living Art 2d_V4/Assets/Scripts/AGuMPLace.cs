f4adc072c897213da07af0437603fe11d7ce6066
PepijnKok <AtlasTheTitan77@gmail.com>
2018-07-25 14:21:32 +0200

    EasyFix

--- Living Art 2d_V4/Assets/Scripts/AGuMPLace.cs
+++ Living Art 2d_V4/Assets/Scripts/AGuMPLace.cs
@@ -5,4 +5,25 @@ using UnityEngine;
 public class AGuMPLace : MonoBehaviour {
 
 	public bool hasGum;
+	private SpriteRenderer SprRnd;
+	private float Transparency = 0.0f;
+	private bool TransparencyUp = false;
+	public int WaitTime = 30;
+	void Start(){
+		SprRnd = GetComponent<SpriteRenderer>();
+		StartCoroutine(WaitForSecond());
+	}
+	void Update(){
+		SprRnd.color = new Color(SprRnd.color.r,SprRnd.color.g,SprRnd.color.b,Transparency);
+		if(TransparencyUp){
+			if(Transparency <= 0.28){
+				Transparency += 0.001f;
+			}
+		}
+	}
+
+	IEnumerator WaitForSecond(){
+		yield return new WaitForSeconds(WaitTime);
+		TransparencyUp = true;
+	}
 }
--- Living Art 2d_V4/Assets/Scripts/TrampolinePlace.cs
+++ Living Art 2d_V4/Assets/Scripts/TrampolinePlace.cs
@@ -6,10 +6,31 @@ public class TrampolinePlace : MonoBehaviour {
 	public GameObject Trampoline;
 	public GameObject Gum;
 	public bool hasGum;
+	private SpriteRenderer SprRnd;
+	private float Transparency = 0.0f;
+	private bool TransparencyUp = false;
+	public int WaitTime = 30;
+	void Start(){
+		SprRnd = GetComponent<SpriteRenderer>();
+		StartCoroutine(WaitForSecond());
+	}
+	void Update(){
+		SprRnd.color = new Color(SprRnd.color.r,SprRnd.color.g,SprRnd.color.b,Transparency);
+		if(TransparencyUp){
+			if(Transparency <= 0.28){
+				Transparency += 0.001f;
+			}
+		}
+	}
 	public void CREATE(){
 		Destroy(Gum);
 		Gum = null;
 		Trampoline.SetActive(true);
 		Destroy(this.gameObject);
+	}
+
+	IEnumerator WaitForSecond(){
+		yield return new WaitForSeconds(WaitTime);
+		TransparencyUp = true;
 	}
 }
